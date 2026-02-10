c593350c91ce67d36e75cc863992cea918317adf
Yerasyl Alimbek <erasylalimbek302@gmail.com>
2026-02-10 23:06:55 +0500

    Initial commit

---
+++ RecipePlanner/RecipePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePlannerApp.swift
+//  RecipePlanner
+//
+//  Created by Yerasyl Alimbek on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct RecipePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipePlannerTests/RecipePlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePlannerTests.swift
+//  RecipePlannerTests
+//
+//  Created by Yerasyl Alimbek on 10.02.2026.
+//
+
+import Testing
+@testable import RecipePlanner
+
+struct RecipePlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
